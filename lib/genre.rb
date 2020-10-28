ae417678854dfc3ca92c200342192a8fe6802f0f
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-10-28 23:38:12 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,22 +1,4 @@
 class Genre
-  attr_accessor :name, :songs
-  @@all = []
 
-  def initialize(name)
-    @name = name
-    self.all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-  def songs
-    @songs = Songs.all.collect {|song| song.genre == self}
-  end
-
-  def artists
-
-  end
 
 end
